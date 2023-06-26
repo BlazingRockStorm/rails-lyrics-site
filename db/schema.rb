55da8874f899479680806dedd993639197b9555f
BlazingRockStorm <rocker.gryphon@gmail.com>
2023-06-26 11:42:32 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,24 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_612_054_342) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_054342) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'genres', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'songs', force: :cascade do |t|
-    t.string 'name'
-    t.text 'lyric'
-    t.bigint 'genre_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['genre_id'], name: 'index_songs_on_genre_id'
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.text "lyric"
+    t.bigint "genre_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["genre_id"], name: "index_songs_on_genre_id"
   end
 
-  add_foreign_key 'songs', 'genres'
+  add_foreign_key "songs", "genres"
 end
