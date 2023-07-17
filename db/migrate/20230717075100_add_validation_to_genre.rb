49fb9c42a94080859b63cdd5ae813b46cd894e19
BlazingRockStorm <rocker.gryphon@gmail.com>
2023-07-17 16:55:41 +0900

    add unique in some fields

---
+++ db/migrate/20230717075100_add_validation_to_genre.rb
@@ -0,0 +1,5 @@
+class AddValidationToGenre < ActiveRecord::Migration[7.0]
+  def change
+    add_index :genres, :name, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_13_110906) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_17_075100) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_13_110906) do
     t.text "biography"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_artists_on_name", unique: true
   end
 
   create_table "artists_songs", id: false, force: :cascade do |t|
@@ -30,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_13_110906) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_genres_on_name", unique: true
   end
 
   create_table "songs", force: :cascade do |t|
