b0a98f887ced829632c90a7f4d041e033053e67d
BlazingRockStorm <rocker.gryphon@gmail.com>
2023-07-17 17:06:04 +0900

    validate in model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,5 +3,5 @@
 class Artist < ApplicationRecord
   has_many :artists_songs, dependent: :destroy
   has_many :songs, through: :artists_songs
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
