2a40178260f802e446481890b424176f7a342be4
BlazingRockStorm <rocker.gryphon@gmail.com>
2023-07-19 19:11:04 +0900

    use puma socket only

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -23,8 +23,10 @@ port ENV.fetch('PORT', 3000)
 #
 environment ENV.fetch('RAILS_ENV', 'development')
 
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
+
+app_root = File.expand_path('../../..', __FILE__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
+stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
@@ -43,7 +45,3 @@ pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
-
-app_root = File.expand_path('../../..', __FILE__)
-bind "unix://#{app_root}/tmp/sockets/puma.sock"
-stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
