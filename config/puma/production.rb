1a494ae0a43292c33d13130568f7c411eb8976a3
BlazingRockStorm <rocker.gryphon@gmail.com>
2023-07-25 22:25:41 +0900

    edit for production

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -11,13 +11,13 @@ min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
 threads min_threads_count, max_threads_count
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
+# terminating a worker in production environments.
 #
-worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
+worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'production') == 'production'
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch('RAILS_ENV', 'development')
+environment ENV.fetch('RAILS_ENV', 'production')
 
 
 app_root = File.expand_path('../../..', __FILE__)
