5170b799fd736b838b09f651bf25bc031aa1baa0
BlazingRockStorm <rocker.gryphon@gmail.com>
2023-07-26 14:17:03 +0900

    add id for models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_26_023526) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_26_051257) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_023526) do
     t.index ["name"], name: "index_artists_on_name", unique: true
   end
 
-  create_table "artists_songs", id: false, force: :cascade do |t|
+  create_table "artists_songs", force: :cascade do |t|
     t.bigint "song_id", null: false
     t.bigint "artist_id", null: false
     t.integer "artist_type"
