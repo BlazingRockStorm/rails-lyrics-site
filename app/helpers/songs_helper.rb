bde079b5fe2f5114d51d3a18542000e3ac19dc9b
BlazingRockStorm <rocker.gryphon@gmail.com>
2023-07-26 15:22:40 +0900

    fix songs_helper

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -2,6 +2,7 @@
 
 module SongsHelper
   def artists_list(artists)
-    artists.joins(:artists_songs).order('artists_songs.artist_type asc').map(&:name).join(' x ')
+    artists = artists.joins(:artists_songs).order('artists_songs.artist_type asc') if artists.count > 1
+    artists.map(&:name).join(' x ')
   end
 end
