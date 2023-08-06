e13335b588c319b9102ee6d93ae53d829c3fec40
BlazingRockStorm <rocker.gryphon@gmail.com>
2023-08-06 23:48:35 +0900

    fix migration

--- db/migrate/20230726051257_add_primary_key_for_artists_song.rb
+++ db/migrate/20230726051257_add_primary_key_for_artists_song.rb
@@ -1,5 +1,5 @@
 class AddPrimaryKeyForArtistsSong < ActiveRecord::Migration[7.0]
   def change
-    add_column :artists_songs, :id, :primary_key
+    add_column :artists_songs, :id, :primary_key, auto_increment: true
   end
 end
