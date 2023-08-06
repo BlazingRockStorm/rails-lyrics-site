cb060ab60dd5afb748a52e8ccb25028c46a1647e
BlazingRockStorm <rocker.gryphon@gmail.com>
2023-08-06 21:48:38 +0900

    order by main and feature artist

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -2,7 +2,7 @@
 
 module SongsHelper
   def artists_list(artists)
-    # artists = artists.joins(:artists_songs).order('artists_songs.artist_type asc') if artists.count > 1
+    artists = artists.order('artists_songs.artist_type asc') if artists.count > 1
     artists.map(&:name).join(' x ')
   end
 end
