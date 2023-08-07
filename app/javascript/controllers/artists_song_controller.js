4a157b58824f53e5720c421eee233a9d612426dc
BlazingRockStorm <rocker.gryphon@gmail.com>
2023-08-07 17:07:50 +0900

    add featuring artists form

---
+++ app/javascript/controllers/artists_song_controller.js
@@ -0,0 +1,14 @@
+// app/javascript/controllers/artists_song_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["fields"];
+
+  addFeaturingField(event) {
+    event.preventDefault();
+    const template = this.fieldsTarget.querySelector(".featuring-field-template");
+    const newField = document.createElement("div");
+    newField.innerHTML = template.innerHTML;
+    this.fieldsTarget.appendChild(newField);
+  }
+}
