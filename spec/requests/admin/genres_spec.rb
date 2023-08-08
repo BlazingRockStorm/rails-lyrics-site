befafdcbd2aae9167a8ee4561d1a4259d21dcffa
BlazingRockStorm <rocker.gryphon@gmail.com>
2023-08-08 17:56:18 +0900

    fix rspec

--- spec/requests/admin/genres_spec.rb
+++ spec/requests/admin/genres_spec.rb
@@ -8,13 +8,37 @@ RSpec.describe 'Admin::Genres' do
   end
 
   describe 'GET #index' do
-    let!(:genres) { create_list(:genre, 3) }
+    describe 'show all genres' do
+      before do
+        stub_const('Admin::GenresController::PAGE_LIMIT', 3)
+        sign_in @admin
+      end
 
-    it 'show all genres' do
-      sign_in @admin
-      get admin_genres_path
-      expect(response).to render_template(:index)
-      expect(response.body).to include(genres[0].name, genres[1].name, genres[2].name)
+      let!(:genres) { create_list(:genre, 4) }
+
+      context 'page 1' do
+        before do
+          get admin_genres_path, params: { page: 1 }
+        end
+
+        it 'show genre 1, genre 2 and genre 3' do
+          expect(response).to render_template(:index)
+          expect(response.body).to include(genres[0].name, genres[1].name, genres[2].name)
+          expect(response.body).not_to include(genres[3].name)
+        end
+      end
+
+      context 'page 2' do
+        before do
+          get admin_genres_path, params: { page: 2 }
+        end
+
+        it 'show genre 4' do
+          expect(response).to render_template(:index)
+          expect(response.body).to include(genres[3].name)
+          expect(response.body).not_to include(genres[0].name, genres[1].name, genres[2].name)
+        end
+      end
     end
   end
 
--- spec/requests/genres_spec.rb
+++ spec/requests/genres_spec.rb
@@ -4,12 +4,36 @@ require 'rails_helper'
 
 RSpec.describe 'Genres' do
   describe 'GET #index' do
-    let!(:genres) { create_list(:genre, 3) }
+    describe 'show all genres' do
+      before do
+        stub_const('GenresController::PAGE_LIMIT', 3)
+      end
 
-    it 'show all genres' do
-      get genres_path
-      expect(response).to render_template(:index)
-      expect(response.body).to include(genres[0].name, genres[1].name, genres[2].name)
+      let!(:genres) { create_list(:genre, 4) }
+
+      context 'page 1' do
+        before do
+          get genres_path, params: { page: 1 }
+        end
+
+        it 'show genre 1, genre 2 and genre 3' do
+          expect(response).to render_template(:index)
+          expect(response.body).to include(genres[0].name, genres[1].name, genres[2].name)
+          expect(response.body).not_to include(genres[3].name)
+        end
+      end
+
+      context 'page 2' do
+        before do
+          get genres_path, params: { page: 2 }
+        end
+
+        it 'show genre 4' do
+          expect(response).to render_template(:index)
+          expect(response.body).to include(genres[3].name)
+          expect(response.body).not_to include(genres[0].name, genres[1].name, genres[2].name)
+        end
+      end
     end
   end
 
