78c11e8d08570bfaa9d880e686414f512b41e4d6
BlazingRockStorm <rocker.gryphon@gmail.com>
2023-08-17 17:26:53 +0900

    add element

--- app/javascript/controllers/artists_songs_controller.js
+++ app/javascript/controllers/artists_songs_controller.js
@@ -2,10 +2,11 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = [ "featuringArtistsList" ]
+  static targets = [ 'featuringArtistsList' ]
 
   addFeaturingArtist() {
-    this.featuringArtistsListTarget.textContent =
-      `Hello, Quan!`
+    const item = document.createElement('p')
+    item.innerHTML = 'Hello' // thay ở đây là 1 cái template gì đó???
+    this.featuringArtistsListTarget.appendChild(item);
   }
 }
