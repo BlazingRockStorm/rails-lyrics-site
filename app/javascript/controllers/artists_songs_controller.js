49f108f09d8f1344a3c85eeb2ff1899c8c827170
BlazingRockStorm <rocker.gryphon@gmail.com>
2023-08-17 19:10:20 +0900

    format document

--- app/javascript/controllers/artists_songs_controller.js
+++ app/javascript/controllers/artists_songs_controller.js
@@ -2,17 +2,17 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = [ 'featuringArtistsList', 'featuringArtist' ]
+  static targets = ['featuringArtistsList', 'featuringArtist']
 
   addFeaturingArtist(event) {
-    event.preventDefault();
-    const template = this.featuringArtistsListTarget.querySelector(".featuring-field-template");
-    const newField = document.createElement('div');
-    newField.innerHTML = template.innerHTML;
-    this.featuringArtistsListTarget.appendChild(newField);
+    event.preventDefault()
+    const template = this.featuringArtistsListTarget.querySelector(".featuring-field-template")
+    const newField = document.createElement('div')
+    newField.innerHTML = template.innerHTML
+    this.featuringArtistsListTarget.appendChild(newField)
   }
 
   removeFeaturingArtist() {
-    this.featuringArtistTarget.remove();
+    this.featuringArtistTarget.remove()
   }
 }
