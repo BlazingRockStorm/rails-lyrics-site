cc3871d0a263ffb502b31140b5dc2bced9a9bdc3
BlazingRockStorm <rocker.gryphon@gmail.com>
2023-08-28 11:42:11 +0900

    edit form

--- app/javascript/controllers/artists_songs_controller.js
+++ app/javascript/controllers/artists_songs_controller.js
@@ -8,6 +8,10 @@ export default class extends Controller {
     event.preventDefault()
     const template = this.featuringArtistsListTarget.querySelector(".featuring-field-template")
     const newField = document.createElement('div')
+    // template.innerHTML.replace('disabled="disabled"', '')
+    // template.getElementsByTagName('select').disabled = false
+    // template.getElementsByTagName('input').disabled = false
+    // console.log(template.getElementsByTagName('select'))
     newField.innerHTML = template.innerHTML
     this.featuringArtistsListTarget.appendChild(newField)
   }
