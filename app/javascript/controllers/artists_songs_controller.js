39564d257c0486939347ea435a583b6bc6bf024f
Hoang Duc Quan <rocker.gryphon@gmail.com>
2023-09-11 21:58:36 +0900

    form disable (#20)
    * form disable
    
    * fix edit form

--- app/javascript/controllers/artists_songs_controller.js
+++ app/javascript/controllers/artists_songs_controller.js
@@ -8,11 +8,7 @@ export default class extends Controller {
     event.preventDefault()
     const template = this.featuringArtistsListTarget.querySelector(".featuring-field-template")
     const newField = document.createElement('div')
-    // template.innerHTML.replace('disabled="disabled"', '')
-    // template.getElementsByTagName('select').disabled = false
-    // template.getElementsByTagName('input').disabled = false
-    // console.log(template.getElementsByTagName('select'))
-    newField.innerHTML = template.innerHTML
+    newField.innerHTML = template.innerHTML.replace('disabled="disabled"', '')
     this.featuringArtistsListTarget.appendChild(newField)
   }
 
