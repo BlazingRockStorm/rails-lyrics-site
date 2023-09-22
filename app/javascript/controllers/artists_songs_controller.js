c7cf82f92816fc792dc9ce969a8426ea1da2e103
Hoang Duc Quan <rocker.gryphon@gmail.com>
2023-09-22 23:39:30 +0900

    Fix edit form (#22)
    * value of the main artist
    
    * current value of edit form
    
    * fix edit form
    
    * fix new form
    
    * fix bug replaceAll
    
    * add current value to form
    
    * fix edit
    
    * fix form

--- app/javascript/controllers/artists_songs_controller.js
+++ app/javascript/controllers/artists_songs_controller.js
@@ -8,11 +8,24 @@ export default class extends Controller {
     event.preventDefault()
     const template = this.featuringArtistsListTarget.querySelector(".featuring-field-template")
     const newField = document.createElement('div')
-    newField.innerHTML = template.innerHTML.replace('disabled="disabled"', '')
-    this.featuringArtistsListTarget.appendChild(newField)
+    newField.innerHTML = template.innerHTML.replaceAll('disabled="disabled"', '')
+    const newTemp = this.featuringArtistsListTarget.appendChild(newField)
+    console.log(newTemp)
+    const uuid = self.crypto.randomUUID()
+    newTemp.id = uuid
+    const removeBtn = newTemp.querySelector("button[type='button']")
+    removeBtn.dataset.artistsSongsTemplateParam = uuid
   }
 
-  removeFeaturingArtist() {
-    this.featuringArtistTarget.remove()
+  removeFeaturingArtist(e) {
+    const featuringArtist = document.getElementById(e.params.template)
+    const input = featuringArtist.querySelector("input[name='song[artists_songs_attributes][][_destroy]']")
+    input.value = '1'
+    const idHiddenElement = featuringArtist.querySelector("input[name='song[artists_songs_attributes][][id]']")
+    if (idHiddenElement) {
+      featuringArtist.hidden = true
+    } else {
+      featuringArtist.remove()
+    }
   }
 }
