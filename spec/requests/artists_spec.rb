6dd9cd19a5e69ccc192c6edcfe91ff7abb3dcd25
BlazingRockStorm <rocker.gryphon@gmail.com>
2023-10-03 14:31:35 +0900

    rewrite tests

--- spec/requests/artists_spec.rb
+++ spec/requests/artists_spec.rb
@@ -38,15 +38,24 @@ RSpec.describe 'Artists' do
   end
 
   describe 'GET #show' do
-    let(:artist) { create(:artist) }
-    let!(:songs) { create_list(:song, 3, artists: [artist]) }
+    context 'find and return an artist' do
+      let(:artist) { create(:artist) }
+      let!(:songs) { create_list(:song, 3, artists: [artist]) }
 
-    it "show a artist's info" do
-      get artist_path(artist)
-      expect(response).to have_http_status(:ok)
-      expect(response.body).to include(artist.name)
-      expect(response.body).to include(artist.biography)
-      expect(response.body).to include(songs[0].name, songs[1].name, songs[2].name)
+      it "show a artist's info" do
+        get artist_path(artist)
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to include(artist.name)
+        expect(response.body).to include(artist.biography)
+        expect(response.body).to include(songs[0].name, songs[1].name, songs[2].name)
+      end
+    end
+
+    context 'not found and return an artist' do
+      it 'not found' do
+        get artist_path(0)
+        expect(response).to have_http_status(:not_found)
+      end
     end
   end
 end
