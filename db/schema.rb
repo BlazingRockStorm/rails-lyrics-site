8446cc826f69e1b881b2a1f786b2b59ff6b9d3c2
BlazingRockStorm <rocker.gryphon@gmail.com>
2023-10-03 14:18:29 +0900

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_11_143018) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_03_025047) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.string "name"
+    t.bigint "artist_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_albums_on_artist_id"
+  end
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -58,5 +66,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_11_143018) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "albums", "artists"
   add_foreign_key "songs", "genres"
 end
