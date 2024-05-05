798324597c822cc9b359d8679c0cdaaefa6f0afc
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-05-05 16:45:48 +0700

    fix elasticsearch bug

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -8,8 +8,3 @@ if File.exist?('config/elasticsearch.yml')
   processed = YAML.safe_load(template.result(binding))
   config.merge!(processed[Rails.env].symbolize_keys)
 end
-
-# if you choose to use elasticsearch-rails-model
-Elasticsearch::Model.client = Elasticsearch::Client.new(config)
-# if you choose to use elasticsearch-rails-persistence
-Elasticsearch::Persistence.client = Elasticsearch::Client.new(config)
