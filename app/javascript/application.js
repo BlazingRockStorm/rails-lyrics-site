8d99e927da76b3dff2a3a0bcb2d58b25c4520ade
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-08-13 15:24:41 +0900

    run rails importmap:install

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,4 @@
-// Entry point for the build script in your package.json
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require app_assets
 import "@hotwired/turbo-rails"
 import "./controllers"
