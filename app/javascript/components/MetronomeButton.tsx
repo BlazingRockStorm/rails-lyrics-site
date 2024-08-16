ade215a680acf61d47da987917fc9aaf6819156d
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-08-16 16:34:26 +0900

    change into React Metronome Button

---
+++ app/javascript/components/MetronomeButton.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MetronomeButton = () => {
+  return (
+    <div>
+      <button className='btn btn-primary'>
+        Start metronome for the song
+      </button>
+    </div>
+  );
+}
+
+export default MetronomeButton
--- app/javascript/components/index.tsx
+++ app/javascript/components/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import MetronomeButton from "./MetronomeButton";
 
 const metronome_container = document.getElementById("metronome-button");
 
@@ -12,7 +13,7 @@ const metronome_root = createRoot(metronome_container);
 document.addEventListener("DOMContentLoaded", () => {
   metronome_root.render(
     <>
-      <p className="font-bold">Yay! You're on Rails!</p>
+      <MetronomeButton />
     </>
   );
 });
