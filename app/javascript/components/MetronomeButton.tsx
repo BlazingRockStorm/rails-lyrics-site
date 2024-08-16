017bac973573285337fd996c4ee89582307476ff
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-08-16 23:18:33 +0900

    Metronome using ReactJS

--- app/javascript/components/MetronomeButton.tsx
+++ app/javascript/components/MetronomeButton.tsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from 'react';
 
-const MetronomeButton = () => {
+interface MetronomeProps {
+  tempo: number;
+  sound: string;
+}
+
+const MetronomeButton = ({ tempo, sound }: MetronomeProps) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const timerRef = useRef<number>(0);
+  const beat = new Audio(sound);
+
+  const handleClick = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  useEffect(() => {
+    if (isPlaying) {
+      timerRef.current = setInterval(() => {
+        beat.play();
+      }, (60 / tempo) * 1000);
+    } else {
+      clearInterval(timerRef.current);
+    }
+    return () => clearInterval(timerRef.current);
+  }, [isPlaying, tempo]);
+
   return (
-    <div>
-      <button className='btn btn-primary'>
-        Start metronome for the song
-      </button>
-    </div>
+    <button className="btn btn-primary" onClick={handleClick}>
+      {isPlaying ? 'Stop metronome for the song' : 'Start metronome for the song'}
+    </button>
   );
 }
 
--- app/javascript/components/index.tsx
+++ app/javascript/components/index.tsx
@@ -8,12 +8,25 @@ if (!metronome_container) {
   throw new Error("Couldn't find metronome element");
 }
 
+// Handle Metronome action
 const metronome_root = createRoot(metronome_container);
+const tempo = metronome_container.getAttribute("data-tempo")
+const sound = metronome_container.getAttribute("data-sound")
+
+if (!tempo) {
+  throw new Error("Couldn't find tempo");
+}
+
+if (!sound) {
+  throw new Error("Couldn't find sound");
+}
+
+const parsedTempo = parseInt(tempo, 10);
 
 document.addEventListener("DOMContentLoaded", () => {
   metronome_root.render(
     <>
-      <MetronomeButton />
+      <MetronomeButton tempo={parsedTempo} sound={sound} />
     </>
   );
 });
