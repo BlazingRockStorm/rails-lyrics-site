c4ea5b14d92f487d3c83311a71ad77623f8b441b
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-08-19 21:43:25 +0900

    add production env check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # rubocop:disable Naming/VariableNumber
+  rescue_from Exception, with: :render_500 if Rails.env.production?
+  rescue_from ActionController::RoutingError, with: :render_404  if Rails.env.production?
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404 if Rails.env.production?
+  # rubocop:enable Naming/VariableNumber
+
   private
 
   def skip_bullet
@@ -10,4 +16,14 @@ class ApplicationController < ActionController::Base
   ensure
     Bullet.enable = previous_value
   end
+
+  # rubocop:disable Naming/VariableNumber
+  def render_404
+    render file: Rails.public_path.join('404.html').to_s, status: :not_found
+  end
+
+  def render_500
+    render file: Rails.public_path.join('500.html').to_s, layout: 'application', content_type: 'text/html'
+  end
+  # rubocop:enable Naming/VariableNumber
 end
